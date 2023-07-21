988cc4a1a21d337d43ac718f7346f89d9bdb330c
Maksim <bobreczovm@inbox.ru>
2023-07-21 11:10:28 +0300

    Загружается перечень пользователей

--- Client/Project/RefUserListPage.xaml.cs
+++ Client/Project/RefUserListPage.xaml.cs
@@ -6,12 +6,17 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Class_interaction_Users;
 //using Xamarin.Forms;
 
 namespace Client.Project
 {
     public partial class RefUserListPage : ContentPage
     {
+
+        public CommandCL command = new CommandCL();
+
+        public List<RefUser> Пользователи_для_теста = new List<RefUser>();
         //public Command EditCommand { get; set; }
         //public Command SelectCommand { get; set; }
         public RefUserListPage()
@@ -22,36 +27,77 @@ namespace Client.Project
             //SelectCommand = new Command(SelectQuestion);
         }
 
+        private void ContentPage_Loaded(object sender, EventArgs e)
+        {
+
+
+            //Application.Current.MainPage.Window.MinimumWidth = 580;
+            //Application.Current.MainPage.Window.MinimumHeight = 400;
+            //Application.Current.MainPage.Window.MaximumWidth = 590;
+            //Application.Current.MainPage.Window.MaximumHeight = 400;
+
+        }
+
+
         // Метод для получения списка вопросов
         private List<RefUser> GetUser()
         {
+
+            Task.Run(async () => await command.From_Friend(Ip_adress.Ip_adresss, "", "015")).Wait();
+
+            if (CommandCL.Regis_Users_Test == null)
+            {
+
+            }
+            else
+            {
+                string[] strings = new string[CommandCL.Regis_Users_Test.regis.Length];
+                for (int i = 0; i < strings.Length; i++)
+                {
+                    strings[i] = CommandCL.Regis_Users_Test.regis[i].Name_Employee;
+                }
+
+
+                for (int i = 0; i < strings.Length; i++)
+                {
+                     var Ref = new RefUser { User = strings[i], EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) };
+                     Пользователи_для_теста.Add(Ref);
+
+                }
+
+                //UserList.ItemsSource = Пользователи_для_теста;
+            }
+
             // Здесь вы можете получить список вопросов из вашего источника данных
             // Возвращаем примерный список вопросов для демонстрации
-            return new List<RefUser>
-            {
-                new RefUser { User = "Пользователь 1", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
-                new RefUser { User = "Пользователь 3", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) }
+            return Пользователи_для_теста;
 
-            };
+
+            //return new List<RefUser>
+            //{
+            //new RefUser { User = "Пользователь 1", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 2", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) },
+            //new RefUser { User = "Пользователь 3", EditCommand = new Command(EditUser), SelectCommand = new Command(SelectUser) }
+
+            //};
         }
 
         private async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
